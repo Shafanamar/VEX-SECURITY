6521b6fe3b2df34ae042d044cc2231f9acbaf2e1
Shafanamar <149698834+Shafanamar@users.noreply.github.com>
2024-10-01 13:32:57 +0530

    connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,1 +1,18 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected successfully.');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
 
