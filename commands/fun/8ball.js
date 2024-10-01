b5f64ef8c85882c8b20489b7ad62b31267c2e718
Shafanamar <149698834+Shafanamar@users.noreply.github.com>
2024-10-01 13:24:27 +0530

    8ball.js

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -1,1 +1,9 @@
+module.exports = {
+    name: 'roll',
+    description: 'Roll a dice',
+    execute(message) {
+        const roll = Math.floor(Math.random() * 6) + 1;
+        message.channel.send(`You rolled a ${roll}! 🎲`);
+    }
+};
 
