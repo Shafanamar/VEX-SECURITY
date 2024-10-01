b3c456d711d13c9ec11ca2d74403545b51fb9095
Shafanamar <149698834+Shafanamar@users.noreply.github.com>
2024-10-01 13:17:13 +0530

    Subscription.js

--- models/Subscription.js
+++ models/Subscription.js
@@ -1,1 +1,19 @@
+const { createEmbed } = require('../utils/embedBuilder');
+
+module.exports = {
+    name: 'subscribe',
+    description: 'View and manage your subscription options',
+    execute(message) {
+        const embed = createEmbed(
+            'Subscription Plans',
+            'Choose from the following subscription tiers:',
+            '#FFD700'
+        )
+        .addField('Bronze Tier', 'Basic anti-raid, customizable welcome messages - $5/month', true)
+        .addField('Silver Tier', 'Everything in Bronze + advanced moderation tools - $10/month', true)
+        .addField('Gold Tier', 'Everything in Silver + priority support & custom embeds - $20/month', true);
+
+        message.channel.send({ embeds: [embed] });
+    },
+};
 
