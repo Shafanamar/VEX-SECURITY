eb8007ef19cafca8d62769386eed4b3974962179
Shafanamar <149698834+Shafanamar@users.noreply.github.com>
2024-10-01 13:27:36 +0530

    ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,1 +1,21 @@
+module.exports = {
+    name: 'ban',
+    description: 'Ban a user',
+    async execute(message, args) {
+        if (!message.member.permissions.has("BAN_MEMBERS")) return message.reply("You don't have permission to do that!");
+
+        const user = message.mentions.users.first();
+        if (user) {
+            const member = message.guild.members.resolve(user);
+            if (member) {
+                await member.ban();
+                message.channel.send(`${user.tag} has been banned.`);
+            } else {
+                message.channel.send("That user isn't in this guild!");
+            }
+        } else {
+            message.channel.send("You didn't mention anyone to ban!");
+        }
+    }
+};
 
