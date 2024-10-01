de2c058220a00fb1d3b57cb83580a84889a6c339
Shafanamar <149698834+Shafanamar@users.noreply.github.com>
2024-10-01 13:30:33 +0530

    messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,1 +1,17 @@
+const prefix = '!'; // Assuming `!` is the command prefix.
+
+module.exports = {
+    name: 'messageCreate',
+    execute(message) {
+        if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+        const commandName = args.shift().toLowerCase();
+
+        const command = message.client.commands.get(commandName);
+        if (command) {
+            command.execute(message, args);
+        }
+    }
+};
 
