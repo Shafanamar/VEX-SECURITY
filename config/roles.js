8ad422140144f1225096b35fdbee8e20ee1c26e5
Shafanamar <149698834+Shafanamar@users.noreply.github.com>
2024-10-01 13:49:43 +0530

    roles.js

--- config/roles.js
+++ config/roles.js
@@ -1,1 +1,33 @@
+module.exports = {
+    bronze: {
+        name: 'Bronze Subscriber', // Role name for Bronze tier
+        permissions: [
+            'VIEW_CHANNEL',
+            'SEND_MESSAGES',
+            'READ_MESSAGE_HISTORY'
+        ],
+        description: 'Basic access with limited features.'
+    },
+    silver: {
+        name: 'Silver Subscriber', // Role name for Silver tier
+        permissions: [
+            'VIEW_CHANNEL',
+            'SEND_MESSAGES',
+            'READ_MESSAGE_HISTORY',
+            'ADD_REACTIONS'
+        ],
+        description: 'Enhanced access with additional features.'
+    },
+    gold: {
+        name: 'Gold Subscriber', // Role name for Gold tier
+        permissions: [
+            'VIEW_CHANNEL',
+            'SEND_MESSAGES',
+            'READ_MESSAGE_HISTORY',
+            'ADD_REACTIONS',
+            'MANAGE_MESSAGES'
+        ],
+        description: 'Full access with all features available.'
+    }
+};
 
