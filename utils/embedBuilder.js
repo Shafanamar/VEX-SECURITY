b72809f3d20abe8a657f5927aa33ff28b2e5a94b
Shafanamar <149698834+Shafanamar@users.noreply.github.com>
2024-10-01 13:32:33 +0530

    embedBuilder.js

--- utils/embedBuilder.js
+++ utils/embedBuilder.js
@@ -1,1 +1,11 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    createEmbed(title, description, color = '#0099ff') {
+        return new MessageEmbed()
+            .setTitle(title)
+            .setDescription(description)
+            .setColor(color);
+    }
+};
 
