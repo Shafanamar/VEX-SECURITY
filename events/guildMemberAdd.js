91a570199a1c67143e011f557b37767828c7a200
Shafanamar <149698834+Shafanamar@users.noreply.github.com>
2024-10-01 13:30:08 +0530

    guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,1 +1,10 @@
+module.exports = {
+    name: 'guildMemberAdd',
+    execute(member) {
+        const channel = member.guild.systemChannel;
+        if (channel) {
+            channel.send(`Welcome to the server, ${member}! Make sure to check out the rules.`);
+        }
+    }
+};
 
