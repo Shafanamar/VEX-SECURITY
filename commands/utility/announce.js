f727e38ce77cdc7b4deb3bdb42dcf4e3ef52a05f
Shafanamar <149698834+Shafanamar@users.noreply.github.com>
2024-10-01 13:29:36 +0530

    announce.js

--- commands/utility/announce.js
+++ commands/utility/announce.js
@@ -1,1 +1,16 @@
+module.exports = {
+    name: 'announce',
+    description: 'Make an announcement in a specified channel',
+    execute(message, args) {
+        if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.reply("You don't have permission to do that!");
+
+        const channel = message.mentions.channels.first();
+        if (!channel) return message.reply("You need to mention a channel!");
+
+        const announcement = args.slice(1).join(" ");
+        if (!announcement) return message.reply("You need to specify the announcement text!");
+
+        channel.send(announcement);
+    }
+};
 
