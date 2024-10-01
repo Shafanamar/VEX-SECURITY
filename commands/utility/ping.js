4c822830c5eb9bcd562da58c56d784a9af7d51db
Shafanamar <149698834+Shafanamar@users.noreply.github.com>
2024-10-01 13:29:04 +0530

    ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,1 +1,10 @@
+module.exports = {
+    name: 'ping',
+    description: 'Check the bot\'s latency',
+    execute(message) {
+        message.channel.send('Pong! 🏓').then(sent => {
+            sent.edit(`Pong! Latency is ${sent.createdTimestamp - message.createdTimestamp}ms.`);
+        });
+    }
+};
 
